db382073c7c582ce60f21f130c4d29978bd3825a
Robert Laverty <roblav96@gmail.com>
2019-11-05 15:31:23 -0500

    suceess panel

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,7 +4,7 @@
 			<div class="columns is-mobile is-gapless is-centered">
 				<div class="column is-narrow">
 					<form
-						class="panel is-link has-background-white-bis"
+						class="panel is-success has-background-white-bis max-w-xs"
 						v-on:submit.prevent="submit"
 					>
 						<p class="panel-heading">Sign Up</p>
